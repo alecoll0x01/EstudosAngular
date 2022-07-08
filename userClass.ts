dcb1e8534dd60142c920de39107d73da684a7fd0
Alessandro Colluci <alessandro.colluci@dnaspecialty.com.br>
2022-07-07 21:56:30 -0300

    first commit

---
+++ userClass.ts
@@ -0,0 +1,28 @@
+class userService{
+    digaOla(){
+        console.log("Ola")
+    }
+}
+
+class Component {
+    constructor(private user: userService) {
+        
+    }
+}
+
+
+class Injector{
+    private _container = new Map();
+
+    constructor(private _providers: any[] = []){
+        this._providers.forEach(service => this._container.set(service, new service()))
+    }
+
+    get(service: any){
+        const serviceInstace = this._container.get(service);
+        if(!service){
+            console.log("Error here!")
+        }
+        return serviceInstace;
+    }
+}
